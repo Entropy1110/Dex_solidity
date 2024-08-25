65ca624f0972e6f535c62eaeee82387e1749695c
Entropy1110 <entropy1110@gmail.com>
2024-08-25 14:03:56 +0900

    init test code

---
+++ src/Dex.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Dex {
+    function addLiquidity(uint256 amountX, uint256 amountY, uint256 minLPReturn) external returns (uint256){
+
+    }
+
+    function removeLiquidity(uint256 liquidity, uint256 minAmountX, uint256 minAmountY) external returns (uint256 amountX, uint256 amountY){
+
+    }
+
+    function swap(uint256 amountX, uint256 amountY, uint256 minAmountOut) external returns (uint256){
+        
+    }
+
+}
