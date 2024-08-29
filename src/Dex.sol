fb4723cf0af9663378466e639eb7f8eabab19e7d
Entropy1110 <entropy1110@gmail.com>
2024-08-30 00:39:12 +0900

    solved - liquidity ratio 제한 구현

--- src/Dex.sol
+++ src/Dex.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.13;
 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract Dex {
+contract Dex is ERC20("Dex", "DEX") {
+
+    event LogUint(uint256 value);
     ERC20 public tokenX;
     ERC20 public tokenY;
     
@@ -19,30 +21,60 @@ contract Dex {
     function addLiquidity(uint256 amountX, uint256 amountY, uint256 minLPReturn) external returns (uint256){
         uint256 LPReturn;
         uint256 reserveX = tokenX.balanceOf(address(this));
-        uint256 reserveY= tokenY.balanceOf(address(this));
+        uint256 reserveY = tokenY.balanceOf(address(this));
         
         require(tokenX.allowance(msg.sender, address(this)) >= amountX, "ERC20: insufficient allowance");
         require(tokenY.allowance(msg.sender, address(this)) >= amountY, "ERC20: insufficient allowance");
         require(tokenX.balanceOf(msg.sender) >= amountX, "ERC20: transfer amount exceeds balance");
         require(tokenY.balanceOf(msg.sender) >= amountY, "ERC20: transfer amount exceeds balance");
 
-        require(amountX > 0 && amountY > 0, "amount must be greater than 0");
+        require(amountX > 0 && amountY > 0, "Amount must be greater than 0");
 
-        if (LPtotalSupply == 0) {
+
+        // 초기 100:200 -> LP=141..
+        // 이후 200:100 -> LP=70.5
+        // min((200 * 141) / 100, (100 * 141) / 200) = 70.5
+
+        // 현재 X 1000 있는 상태
+        // 초기 3000:4000 -> LP=3464..
+        // 이후 X 1000 추가
+        // 이후 5000:4000 -> LP=1732
+        // min((5000 * 3464) / 10000, (4000 * 3464) / 8000) = 1732
+
+        if (LPtotalSupply == 0) { // for the first liquidity provider
             LPReturn = sqrt(amountX * amountY);
-        } else {
-            LPReturn = min((amountX * LPtotalSupply) / reserveX, (amountY * LPtotalSupply) / reserveY);
-        }
-        require(LPReturn >= minLPReturn, "slippage limit reached");
-        LPtotalSupply += LPReturn;
-        LPbalance[msg.sender] += LPReturn;
+            LPtotalSupply += LPReturn;
+            LPbalance[msg.sender] += LPReturn;
+        } else { // same ratio as the pool's ratio
+            uint a = (amountX * LPtotalSupply) / reserveX;
+            uint b = (amountY * LPtotalSupply) / reserveY;
 
+            bool isXSmaller = false;
+
+            if (a < b)
+                isXSmaller = true;
+            
+            LPReturn = isXSmaller ? a : b;
+            LPbalance[msg.sender] += LPReturn;
+
+            if (isXSmaller) 
+                amountY = (LPReturn * reserveY) / LPtotalSupply;
+             else 
+                amountX = (LPReturn * reserveX) / LPtotalSupply;
+            
+            LPtotalSupply += LPReturn;
+        }
+
+        require(LPReturn >= minLPReturn, "Slippage limit reached");
+
+        _mint(msg.sender, LPReturn);
         
 
         tokenX.transferFrom(msg.sender, address(this), amountX);
+        emit LogUint(amountX);
         tokenY.transferFrom(msg.sender, address(this), amountY);
+        emit LogUint(amountY);
 
-        
         return LPReturn;
     }
 
@@ -50,15 +82,17 @@ contract Dex {
         uint256 reserveX = tokenX.balanceOf(address(this));
         uint256 reserveY= tokenY.balanceOf(address(this));
         
-        require(LPbalance[msg.sender] >= liquidity, "insufficient LP balance");
-        require(liquidity > 0, "liquidity must be greater than 0");
+        require(LPbalance[msg.sender] >= liquidity, "Insufficient LP balance");
+        require(liquidity > 0, "Liquidity must be greater than 0");
         
         amountX = (liquidity * reserveX) / LPtotalSupply;
         amountY = (liquidity * reserveY) / LPtotalSupply;
-        require(amountX >= minAmountX && amountY >= minAmountY, "slippage limit reached");
+        require(amountX >= minAmountX && amountY >= minAmountY, "Slippage limit reached");
 
         LPtotalSupply -= liquidity;
         LPbalance[msg.sender] -= liquidity;
+
+        _burn(msg.sender, liquidity);
 
         tokenX.transfer(msg.sender, amountX);
         tokenY.transfer(msg.sender, amountY);
